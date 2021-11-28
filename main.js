cd4ba2e79c425c20a320b0fa85d9d454a5f613eb
stephaniejchan8 <stephaniejchan8@gmail.com>
2021-11-28 18:32:07 +1000

    Second commit - accessible version

--- main.js
+++ main.js
@@ -42,9 +42,121 @@ https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-co
 // Variable1, listen to it for a click
 // function() {Listener would update the image in the screen}
 
+// Create JS variables of DOM elements - channel buttons and tv image screen
+const button1 = document.querySelector('#one');
+const button2 = document.querySelector('#two');
+const button3 = document.querySelector('#three');
+const button4 = document.querySelector('#four');
+const button5 = document.querySelector('#five');
+const button6 = document.querySelector('#six');
+const button7 = document.querySelector('#seven');
+const button8 = document.querySelector('#eight');
+const button9 = document.querySelector('#nine');
+const tv = document.querySelector('.tv-show img');
 
+// Create event listeners for all buttons for mouse click and certain keydown events (checked with seaparate function keyboardInteraction below), to call updateChannel function
+button1.addEventListener('click', updateChannel);
+button1.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+button2.addEventListener('click', updateChannel);
+button2.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+button3.addEventListener('click', updateChannel);
+button3.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+button4.addEventListener('click', updateChannel);
+button4.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+button5.addEventListener('click', updateChannel);
+button5.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+button6.addEventListener('click', updateChannel);
+button6.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+button7.addEventListener('click', updateChannel);
+button7.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+button8.addEventListener('click', updateChannel);
+button8.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+button9.addEventListener('click', updateChannel);
+button9.addEventListener('keydown', function (e) {
+  if (keyboardInteraction(e) === true) {
+    updateChannel(e);
+  }
+});
+
+// Create function to check if keyboard button pressed was enter or space, if so return true so that updateChannel function will run in the above event listeners
+function keyboardInteraction(e) {
+  if (e.keyCode === 13 || e.keyCode === 32) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Create function to switch image portrayed on TV, based on the channel button interacted with by the user
+function updateChannel(e) {
+  switch (e.target.textContent) {
+    case "1":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg";
+      break;
+    case "2":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg";
+      break;
+    case "3":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg";
+      break;
+    case "4":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg";
+      break;
+    case "5":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg";
+      break;
+    case "6":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg";
+      break;
+    case "7":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+      break;
+    case "8":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg";
+      break;
+    case "9":
+      tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg";
+      break;
+    default:
+      console.log("Channel not selected.");
+  }
+}
+
+// REMNANTS OF OLD SOLUTION TO EXERCISE. 
+// Old solution did not incorporate accessibility (keyboard interactivity), also made code more readable
 // let channelSelected;
-// const tv = document.querySelector('.tv-show img');
 // document.querySelector('#one').addEventListener('click', function (e) {
 //   channelSelected = e.target.textContent;
 //   changeChannel(channelSelected);
@@ -89,44 +201,12 @@ https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-co
 //   channelSelected = e.target.textContent;
 //   changeChannel(channelSelected);
 // });
-
-document.querySelector('#one').addEventListener('click', updateChannel);
-
-function updateChannel(e) {
-  switch (e.target.textContent) {
-    
-  }
-}
-
 // function changeChannel(channelNo) {
 //   switch (channelNo) {
 //     case "1":
 //       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg";
 //       break;
-//     case "2":
-//       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg";
-//       break;
-//     case "3":
-//       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg";
-//       break;
-//     case "4":
-//       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg";
-//       break;
-//     case "5":
-//       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg";
-//       break;
-//     case "6":
-//       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg";
-//       break;
-//     case "7":
-//       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
-//       break;
-//     case "8":
-//       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg";
-//       break;
-//     case "9":
-//       tv.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg";
-//       break;
+    
 //     default:
 //       console.log("Channel not selected.");
 //   }
